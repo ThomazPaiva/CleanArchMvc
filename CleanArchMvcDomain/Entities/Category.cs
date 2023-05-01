5e88b01516aba993a0355c1ca8e5891fbb9e85fb
Thomaz de Moura Paiva <tmoura@4bio.com.br>
2023-05-01 17:23:15 -0300

    Add project files.

---
+++ CleanArchMvcDomain/Entities/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace CleanArchMvcDomain.Entities
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public List<Product> Products { get; set; }
+    }
+}
