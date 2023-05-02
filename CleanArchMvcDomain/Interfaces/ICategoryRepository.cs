5d6e33a5ea30c992f754a24c1a1a1796197e2ab5
Thomaz de Moura Paiva <tmoura@4bio.com.br>
2023-05-02 17:17:58 -0300

    [feature] criando as assinaturas dos métodos para obter todos, obter por id, criar, atualizar e deletar uma categoria

---
+++ CleanArchMvcDomain/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchMvcDomain.Entities;
+
+namespace CleanArchMvcDomain.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        Task<List<Category>> GetCategories();
+
+        Task<Category> GetById(int? id);
+
+        Task<Category> Create(Category category);
+
+        Task<Category> Update(Category category);
+
+        Task<Category> Delete(Category category);
+    }
+}
---
+++ CleanArchMvcDomain/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+﻿using CleanArchMvcDomain.Entities;
+
+namespace CleanArchMvcDomain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<List<Product>> GetProductsAsync();
+
+        Task<Product> GetByIdAsync(int? id);
+
+        Task<Product> CreateAsync(Product category);
+
+        Task<Product> UpdateAsync(Product category);
+
+        Task<Product> DeleteAsync(Product category);
+    }
+}
